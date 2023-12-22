dc7e69583247d9481ae7fc930b3c9446aa84a4bc
Albert Mpepo <albertmpepo@gmail.com>
2023-12-22 23:09:38 +0200

    Basic Flask application

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from markupsafe import escape
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    new_text = escape(text).replace("_", " ")
+    return "C {}".format(new_text)
+
+@app.route("/python/", defaults={'text': "is cool"}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_text(text):
+    new_text = escape(text).replace("_", " ")
+    return "Python {}".format(new_text)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
