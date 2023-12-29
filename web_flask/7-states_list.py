4dbf18609824a6880fb7c6548402dc0ca7d24d37
Albert Mpepo <albertmpepo@gmail.com>
2023-12-29 16:58:27 +0200

    8. List of states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+A script that starts a Flask web application:
+    -> web application must be listening on 0.0.0.0, port 5000
+    -> Routes:
+        /states_list: display a HTML page: (inside the tag BODY)
+            => H1 tag: “States”
+            => UL tag: with the list of all State objects present 
+               in DBStorage sorted by name (A->Z)
+                => LI tag: description of one State: <state.id>: <B><state.name></B>
+"""
+from flask import Flask, render_template
+from sqlalchemy.sql.expression import text
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """
+    display a HTML page:
+        H1 tag: 'State'
+        UL tag: 'List of states in DBStorage'
+    """
+    all_data = storage.all()
+    sorted_states = sorted(all_data.values(), key=lambda state: state.name)
+
+    return render_template('7-states_list.html', states=sorted_states)
+
+
+@app.teardown_appcontext
+def teardown_storage(exception):
+    """ Close the storage session after each request """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
